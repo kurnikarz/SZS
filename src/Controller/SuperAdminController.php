32fb21a70da566386cf9efcb8dae4c57c0e5afba
Marcin <Marcingalazka79@gmail.com>
2020-03-10 01:20:26 +0100

    Working SuperAdmin

---
+++ src/Controller/SuperAdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SuperAdminController extends AbstractController
+{
+    /**
+     * @Route("/SuperAdmin", name="super_admin")
+     */
+    public function index()
+    {
+        return $this->render('SuperAdmin/index.html.twig', [
+            'controller_name' => 'SuperAdminController',
+            'name' => 'Marcin Gałązka'
+        ]);
+    }
+}

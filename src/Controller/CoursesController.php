91d069f8f3e9a1f224f252d46462bc5b08a5593f
unknown <wiktor1474@wp.pl>
2020-04-18 18:27:03 +0200

    showing members doesnt work

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkExtraBundle\Configuration\Method;
 use App\Entity\Courses;
 use App\Entity\Trainer;
-
+use App\Entity\Member;
 
 class CoursesController extends AbstractController{
 
@@ -15,9 +15,18 @@ class CoursesController extends AbstractController{
      * @Route("/courses", name="courses")
      */
     public function index(){
-
         $courses = $this->getDoctrine()->getRepository(Courses::class)->findAll();
 
         return $this->render('courses/courses.html.twig', array('courses' => $courses));
+    }
+
+    /**
+     * Route("/courses/{id}", name="members")
+     * @Method({"GET"})
+     */
+    public function show($id){
+        $members = $this->getDoctrine()->getRepository(Member::class)->find($id);
+
+        return $this->render('courses/members.html.twig', array('members' => $members));
     }
 }

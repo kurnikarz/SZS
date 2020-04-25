01e31aef352662b4b2b910a39e741a0ddbabb3f3
unknown <wiktor1474@wp.pl>
2020-04-25 18:06:47 +0200

    trainer sees memebers of courses

--- src/Controller/MembersController.php
+++ src/Controller/MembersController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Member;
 use App\Entity\Training;
-use App\Entity\Trainer;
 
 
 class MembersController extends AbstractController
@@ -16,9 +15,8 @@ class MembersController extends AbstractController
      */
     public function index()
     {
-        $trainers = $this->getDoctrine()->getRepository(Trainer::class)->findAll();
         $members = $this->getDoctrine()->getRepository(Member::class)->findAll();
-        $training = $this->getDoctrine()->getRepository(Training::class)->findAll();
-        return $this->render('members/index.html.twig', array('members' => $members,'training' => $training, 'trainers' => $trainers));
+        $trainings = $this->getDoctrine()->getRepository(Training::class)->findAll();
+        return $this->render('members/index.html.twig', array('members' => $members,'trainings' => $trainings));
     }
 }

1f87810a734bf43ad7d69365caac78d07d833206
artur <dumcio.97@wp.pl>
2020-05-07 20:37:39 +0200

    fix member login

--- src/Security/AppMemberAuthenticator.php
+++ src/Security/AppMemberAuthenticator.php
@@ -96,7 +96,7 @@ class AppMemberAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         return new RedirectResponse($this->urlGenerator->generate('training_list'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()

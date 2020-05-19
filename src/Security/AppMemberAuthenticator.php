1680eafc4b5e91fbee7e7f2676a1c26cf0c22c2d
artur <dumcio.97@wp.pl>
2020-05-19 18:04:20 +0200

    secu

--- src/Security/AppMemberAuthenticator.php
+++ src/Security/AppMemberAuthenticator.php
@@ -69,7 +69,7 @@ class AppMemberAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Nieprawidłowy adres email');
         }
 
         return $user;
